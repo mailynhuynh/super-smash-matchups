c55e4504ba1b62e3da6361df20f7ee67775d49ab
Kamsiy <kamsiyoib@gmail.com>
2019-01-13 02:12:52 -0500

    Update data

--- src/data/CharacterGenerator.java
+++ src/data/CharacterGenerator.java
@@ -18,7 +18,7 @@ public class CharacterGenerator {
 		"Incineroar", "Jiggly Puff", "Lucario", "Mewtwo", "Pichu", "Pikachu", "Ivysaur", "Squirtle",
 		"Charizard", "Pokemon Trainer", "Little Mac", "R.O.B.", "Sonic", "Falco", "Fox", "Wolf", "Ken", 
 		"Ryu", "Mii Brawler", "Mii Gunner", "Mii Swordfighter", "Wario", "Shulk", "Yoshi", "Ganondorf", 
-		"Link", "Sheik", "Toon LInk", "Young Link", "Zelda"
+		"Link", "Sheik", "Toon Link", "Young Link", "Zelda"
 	};
 	
 	public static void main(String[] args) throws IOException {
@@ -37,25 +37,25 @@ public class CharacterGenerator {
 			
 			curr.put("tier", tier.substring(0, 1));
 			curr.put("weight", getStat(doc.select(".hor_line").get(0).toString()));
-			curr.put("air-speed", getStat(doc.select(".hor_line").get(1).toString()));
-			curr.put("fall-speed", getStat(doc.select(".hor_line").get(2).toString()));
-			curr.put("run-speed", getStat(doc.select(".hor_line").get(3).toString()));
-			curr.put("dash-speed", getStat(doc.select(".hor_line").get(4).toString()));
+			curr.put("air_speed", getStat(doc.select(".hor_line").get(1).toString()));
+			curr.put("fall_speed", getStat(doc.select(".hor_line").get(2).toString()));
+			curr.put("run_speed", getStat(doc.select(".hor_line").get(3).toString()));
+			curr.put("dash_speed", getStat(doc.select(".hor_line").get(4).toString()));
 			
 			// unlock
 			JSONArray unlock = new JSONArray();
 			Elements result = doc.select(".generic-li");
 			unlock.put(new JSONObject().put("wofl", getUnlockMethod(result.get(0).text())));
-			unlock.put(new JSONObject().put("classic-mode", getUnlockMethod(result.get(1).text())));
-			unlock.put(new JSONObject().put("vs-mode", getUnlockMethod(result.get(2).text())));
+			unlock.put(new JSONObject().put("classic_mode", getUnlockMethod(result.get(1).text())));
+			unlock.put(new JSONObject().put("vs_mode", getUnlockMethod(result.get(2).text())));
 			
 			// Spirits
 			JSONArray spirits = new JSONArray();
 			Elements spiritList = doc.select(".th-extra");
-			spirits.put(new JSONObject().put("primary", spiritList.get(1).text()));
-			spirits.put(new JSONObject().put("slot-1", spiritList.get(3).text()));
-			spirits.put(new JSONObject().put("slot-2", spiritList.get(5).text()));
-			spirits.put(new JSONObject().put("slot-3", spiritList.get(7).text()));
+			spirits.put(new JSONObject().put("primary", getSpirit(spiritList, 1)));
+			spirits.put(new JSONObject().put("slot_1", getSpirit(spiritList, 3)));
+			spirits.put(new JSONObject().put("slot_2", getSpirit(spiritList, 5)));
+			spirits.put(new JSONObject().put("slot_3", getSpirit(spiritList, 7)));
 			
 			// 
 			// Moves
@@ -69,12 +69,12 @@ public class CharacterGenerator {
 			array.put(curr);
 		}
 		System.out.println("Character Generation Complete...");
-		System.out.println("Writing to Character.json........");
+		System.out.println("Writing to characters.json........");
 		
-		object.put("characters", array);
-		try(FileWriter file = new FileWriter("./Characters.json")) {
+		object.put("character", array);
+		try(FileWriter file = new FileWriter("./characters.json")) {
 			file.write(object.toString());
-			System.out.println("Copied to Characters.json");
+			System.out.println("Copied to characters.json");
 		}
 		
 	}
@@ -83,6 +83,9 @@ public class CharacterGenerator {
 		String result = "";
 		for(int i = str.indexOf("-") + 2; i < str.length(); i++ ) {
 			result += str.charAt(i);
+		}
+		if(result.length() == 0) {
+			return "-";
 		}
 		return result;
 	}
@@ -146,6 +149,14 @@ public class CharacterGenerator {
 		}
 		
 		return baseLink;
+	}
+	
+	public static String getSpirit(Elements spiritList, int position){
+		String result = spiritList.get(position).text();
+		if(result.length() == 0) {
+			return "-";
+		}
+		return result;
 	}
 }
 
